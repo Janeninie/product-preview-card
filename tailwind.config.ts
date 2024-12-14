dfa9d211f2e7864af05e164c639225611fe3eb98
Jane <apiwatsupaphol@gmail.com>
2024-12-14 23:53:48 +0700

    feat: added page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,20 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          darkCyan: "hsl(158, 36%, 37%)",
+          cream: "hsl(30, 38%, 92%)",
+        },
+        neutral: {
+          veryDarkBlue: "hsl(212, 21%, 14%)",
+          darkGrayishBlue: "hsl(228, 12%, 48%)",
+          white: "hsl(0, 0%, 100%)",
+        },
       },
+    },
+    fontFamily: {
+      montserrat: ["Montserrat", "sans-serif"],
+      fraunces: ["Fraunces", "serif"],
     },
   },
   plugins: [],
